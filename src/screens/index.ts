515e9e81515299cd80ff20a3e22dbb6fe910be53
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:28:13 +0700

    Create index.ts

---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+// screens/index.ts
+export { default as HomeScreen } from "./HomeScreen";
+export { default as ActivityScreen } from "./ActivityScreen";
+export { default as QRScannerScreen } from "./QRScannerScreen";
+export { default as DemoDebugScreen } from "./DemoDebugScreen";
