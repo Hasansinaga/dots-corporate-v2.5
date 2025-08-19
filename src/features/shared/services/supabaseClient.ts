828eb4fa9db1bccbc2b9cd7c1b13d73036af73a0
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:21:31 +0700

    Update supabaseClient.ts

--- src/features/shared/services/supabaseClient.ts
+++ src/features/shared/services/supabaseClient.ts
@@ -3,14 +3,14 @@ import 'react-native-url-polyfill/auto'
 
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { ENV } from '@/config/env'
+import { ENV } from "../../../config/env"
 
 export const supabase = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY, {
   auth: {
-    storage: AsyncStorage,       // simpan sesi di AsyncStorage
+    storage: AsyncStorage,     
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,   // RN tidak pakai URL callback
+    detectSessionInUrl: false,  
   },
 })
 
