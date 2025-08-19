e4606c16af69d7894b0724d6c621f5781bcbc29e
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:17:27 +0700

    Update Logo.tsx
    perbaikan logo pada halaman login

--- src/features/shared/components/Logo.tsx
+++ src/features/shared/components/Logo.tsx
@@ -1,8 +1,6 @@
-import { useEffect } from "react"
 import { Image, ImageStyle } from "react-native"
 
-export function Logo({ width = 160, height = 100 }: { width?: number; height?: number }) {
+export function Logo({ width = 260, height = 200 }: { width?: number; height?: number }) {
   const style: ImageStyle = { width, height, resizeMode: "contain" }
-  // Pastikan file ada di assets/images/dkn.png
-  return <Image source={require("@/assets/images/dkn.png")} style={style} />
+  return <Image source={require("../../../../src/assets/images/dots_logo.png")} style={style} />
 }
