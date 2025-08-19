3b8f82e68bcf906584e1fa1760a096d0e4461739
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:23:21 +0700

    Create AppNavigator.tsx

---
+++ src/navigators/AppNavigator.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import CustomerListScreen from '../screens/customers/CustomerListScreen';
+
+export type RootStackParamList = {
+  DemoShowroom: undefined;
+  DaftarNasabah: undefined;  
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="DemoShowroom" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="DaftarNasabah" component={CustomerListScreen} options={{ title: 'Daftar Nasabah' }} />
+    </Stack.Navigator>
+  );
+}
