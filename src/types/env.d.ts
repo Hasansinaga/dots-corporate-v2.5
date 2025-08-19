9b31184ea2b37860f1740869934cde09952982d0
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:28:47 +0700

    Update env.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,9 @@
 declare module '@env' {
-  export const APPCENTER_TOKEN: string
-  export const API_URL: string
-  export const ONESIGNAL_APP_ID: string
-  export const GOOGLE_API_KEY: string
-  export const EXPO_PUBLIC_SUPABASE_URL: string
-  export const EXPO_PUBLIC_SUPABASE_ANON_KEY: string
+  export const APPCENTER_TOKEN: string;
+  export const EXPO_PUBLIC_SUPABASE_URL: string;
+  export const EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
+  export const GOOGLE_API_KEY: string;
+  export const ONESIGNAL_APP_ID: string;
+  export const API_URL: string;
+  export const VERSION_APP: string;
 }
