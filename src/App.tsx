f574f8d039a32a0195c7d9a7ca684b3188c92c55
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:09:29 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-// src/App.tsx
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import LoginScreen from "./features/auth/screens/LoginScreen";
-import HomeTabs from "./navigators/HomeTabs"; // ⬅️ default import (tanpa kurung kurawal)
-import CustomerListScreen from "./screens/customers/CustomerListScreen"; // ⬅️ rute luar tabs
+import HomeTabs from "./navigators/HomeTabs";
+import CustomerListScreen from "./features/customers/screens/CustomerListScreen";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Login: undefined;
+  HomeTabs: undefined;
+  DaftarNasabah: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
