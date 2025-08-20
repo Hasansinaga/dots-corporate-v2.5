9bca47898839319328fccfe119ca6c4231221864
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:11:05 +0700

    Delete searchSavingsCustomers.ts

--- src/features/shared/services/usecases/searchSavingsCustomers.ts
+++
@@ -1,4 +0,0 @@
-import { CustomerRepository } from "../repositories/customer.repository";
-export const searchSavingsCustomers =
-  (repo: CustomerRepository) => (q: string) =>
-    repo.searchSavingsCustomers(q);
