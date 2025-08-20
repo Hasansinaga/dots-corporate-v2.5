2dd45c34225ac166e901a03b97d80aa803de0787
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:11:53 +0700

    Perbaikan Struktur

--- src/screens/index.ts
+++
@@ -1,5 +0,0 @@
-// screens/index.ts
-export { default as HomeScreen } from "./HomeScreen";
-export { default as ActivityScreen } from "./ActivityScreen";
-export { default as QRScannerScreen } from "./QRScannerScreen";
-export { default as DemoDebugScreen } from "./DemoDebugScreen";
