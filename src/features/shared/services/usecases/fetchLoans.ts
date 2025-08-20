3ac5db69245596f3f95c7493460f4ffe003fdf63
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:10:58 +0700

    Delete fetchLoans.ts

--- src/features/shared/services/usecases/fetchLoans.ts
+++
@@ -1,4 +0,0 @@
-import { CustomerRepository } from "../repositories/customer.repository";
-export const fetchLoans =
-  (repo: CustomerRepository) => (filters?: { collectibility?: string[] }) =>
-    repo.fetchCreditCustomers(filters);
