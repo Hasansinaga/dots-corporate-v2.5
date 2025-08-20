04ca456fc4094bc775475fa84fbb9b96f1955e5e
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:11:13 +0700

    Update types.ts

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,24 +1,13 @@
-import type { NativeStackScreenProps } from "@react-navigation/native-stack"
-import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
-import type { CompositeScreenProps } from "@react-navigation/native"
-
 export type AppStackParamList = {
-  Login: undefined
-  Main: undefined
-}
-export type AppStackScreenProps<T extends keyof AppStackParamList> =
-  NativeStackScreenProps<AppStackParamList, T>
+  Login: undefined;
+  HomeTabs: undefined;
+  DaftarNasabah: undefined;
+};
 
-export type DemoTabParamList = {
-  DemoShowroom: undefined
-  DemoActivity: undefined
-  Scan: undefined
-  DemoPodcastList: undefined
-  DemoDebug: undefined
-}
-
-export type DemoTabScreenProps<T extends keyof DemoTabParamList> =
-  CompositeScreenProps<
-    BottomTabScreenProps<DemoTabParamList, T>,
-    AppStackScreenProps<keyof AppStackParamList>
-  >
+export type TabParamList = {
+  DemoShowroom: undefined;
+  DemoActivity: undefined;
+  Scan: undefined;
+  DemoPodcastList: undefined;
+  DemoDebug: undefined;
+};
