12fca61fbf021d8155eedc09dc1382f959db2d3b
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-20 17:11:02 +0700

    Delete searchCreditCustomers.ts

--- src/features/shared/services/usecases/searchCreditCustomers.ts
+++
@@ -1,4 +0,0 @@
-import { CustomerRepository } from "../repositories/customer.repository";
-export const searchCreditCustomers =
-  (repo: CustomerRepository) => (q: string, filters?: { collectibility?: string[] }) =>
-    repo.searchCreditCustomers(q, filters);
